5cb432a83629bffc1c0805fa54edc3c2fc3321fc
lemon <guojingandmeng@gmail.com>
2017-08-30 16:01:08 +0800

    mock api

--- src/main/java/com/guojing/boot/controller/MainController.java
+++ src/main/java/com/guojing/boot/controller/MainController.java
@@ -1,11 +1,15 @@
 package com.guojing.boot.controller;
 
 import com.guojing.boot.entity.Person;
+import com.guojing.boot.entity.TbUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +21,17 @@ import java.util.List;
 @RequestMapping("/")
 public class MainController {
 
+    @RequestMapping("/main/{id}")
+    public ModelAndView view(@PathVariable("id") Long id, HttpServletRequest req) {
+        TbUser user = new TbUser();
+        user.setId(id);
+        user.setLoginName("zhang");
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("user", user);
+        mv.setViewName("main/test");
+        return mv;
+    }
 
     @RequestMapping("/hello")
     public String helloHtml(HashMap<String,Object> map){
--- src/test/java/com/guojing/boot/ApplicationTests2.java
+++ src/test/java/com/guojing/boot/ApplicationTests2.java
@@ -1,69 +1,53 @@
 package com.guojing.boot;
 
+import com.guojing.boot.config.WebMVCConfig;
+import com.guojing.boot.controller.MainController;
 import com.guojing.boot.controller.UserController;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-public class ApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration(value = "src/main/webapp")
+@ContextHierarchy({
+        @ContextConfiguration(name = "parent", classes = WebMVCConfig.class),
+})
+public class ApplicationTests2 {
  
-	private MockMvc mvc;
+	private MockMvc mockMvc;
  
 	@Before
     public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
-	} 
- 
+		mockMvc = MockMvcBuilders.standaloneSetup(new MainController()).build();
+	}
+
 	@Test
-	public void testUserController() throws Exception { 
-        // 测试UserController 
-		RequestBuilder request = null;
- 
-		// 1、get查一下user列表，应该为空 
-		request = get("/users/"); 
-		mvc.perform(request) 
-				.andExpect(status().isOk()) 
-				.andExpect(content().string(equalTo("[]"))); 
- 
-		// 2、post提交一个user 
-		request = post("/users/") 
-				.param("id", "1") 
-				.param("name", "测试大师") 
-				.param("age", "20"); 
-		mvc.perform(request) 
-		        .andExpect(content().string(equalTo("success"))); 
- 
-		// 3、get获取user列表，应该有刚才插入的数据 
-		request = get("/users/"); 
-		mvc.perform(request) 
-				.andExpect(status().isOk()) 
-				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]"))); 
- 
-		// 4、put修改id为1的user 
-		request = put("/users/1") 
-				.param("name", "测试终极大师") 
-				.param("age", "30"); 
-		mvc.perform(request) 
-				.andExpect(content().string(equalTo("success"))); 
- 
-		// 5、get一个id为1的user 
-		request = get("/users/1"); 
-		mvc.perform(request) 
-				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}"))); 
- 
-		// 6、del删除id为1的user 
-		request = delete("/users/1"); 
-		mvc.perform(request) 
-				.andExpect(content().string(equalTo("success"))); 
- 
-		// 7、get查一下user列表，应该为空 
-		request = get("/users/"); 
-		mvc.perform(request) 
-				.andExpect(status().isOk()) 
-				.andExpect(content().string(equalTo("[]"))); 
- 
-	} 
- 
+	public void testView() throws Exception {
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/main/1"))
+				.andExpect(MockMvcResultMatchers.view().name("main/test"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("user"))
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn();
+
+		Assert.assertNotNull(result.getModelAndView().getModel().get("user"));
+	}
+
 }
